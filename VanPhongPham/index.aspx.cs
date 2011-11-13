247703b269f3ec786ab714f56a1ae650fd4368b2
vinhtruongcong@gmail.com <vinhtruongcong@gmail.com@fbb1e87e-8031-b629-a0f2-289ab1b77be4>
2011-11-13 17:29:28 +0000

    Chuc nang download file (báo giá)

--- VanPhongPham/index.aspx.cs
+++ VanPhongPham/index.aspx.cs
@@ -10,11 +10,48 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
-public partial class _Default : System.Web.UI.Page 
+public partial class _Default : System.Web.UI.Page
 {
+    //Lay ve path cua root server
+    string RootPath = HttpContext.Current.Server.MapPath("~");
+
     protected void Page_Load(object sender, EventArgs e)
     {
         //test.Text = CommonDb.OpenConnection().ToString();
         test.Text = CommonUtil.EscapeHTML("<html></html>");
+    }
+
+    /// <summary>
+    /// Download bao gia...
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Download_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            System.String filename = "tencongty_baogia.xls";
+
+            // set the http content type to "APPLICATION/OCTET-STREAM
+            Response.ContentType = "APPLICATION/OCTET-STREAM";
+
+            // initialize the http content-disposition header to
+            // indicate a file attachment with the default filename
+            // "myFile.txt"
+            System.String disHeader = "Attachment; Filename=\"" + filename +
+               "\"";
+            Response.AppendHeader("Content-Disposition", disHeader);
+            
+            // transfer the file byte-by-byte to the response object
+            System.IO.FileInfo fileToDownload = new
+               System.IO.FileInfo(RootPath + "/files/baogia/" + filename);
+            Response.Flush();
+            Response.WriteFile(fileToDownload.FullName);
+        }
+        catch (Exception ex)
+        // file IO errors
+        {
+            //e.StackTrace;
+        }
     }
 }
