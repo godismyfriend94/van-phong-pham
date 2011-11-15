863360171e51f59c52c73e9fdce41c4d9340e8c2
baodinh86@gmail.com <baodinh86@gmail.com@fbb1e87e-8031-b629-a0f2-289ab1b77be4>
2011-11-15 15:33:57 +0000

    display top 5 products

--- trunk/VanPhongPham/vpp/default.aspx.cs
+++ trunk/VanPhongPham/vpp/default.aspx.cs
@@ -11,4 +11,15 @@ public partial class vpp_default : System.Web.UI.Page
     {
 
     }
+    [System.Web.Services.WebMethod]
+    [System.Web.Script.Services.ScriptMethod]
+    public static AjaxControlToolkit.Slide[] GetSlides()
+    {
+        AjaxControlToolkit.Slide[] imgSlide = new AjaxControlToolkit.Slide[4];
+
+        imgSlide[0] = new AjaxControlToolkit.Slide("images/camera.png", "Camera", "Camera made in China fake 1");
+        imgSlide[1] = new AjaxControlToolkit.Slide("images/laptop.png", "laptop", "laptop made in Malaysia , pin 5 cell");
+        imgSlide[2] = new AjaxControlToolkit.Slide("images/iphone.png", "iphone", "iphone 4gs white and luxury");        
+        return (imgSlide);
+    }
 }
